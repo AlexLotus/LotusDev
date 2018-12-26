e56a2c573a37dc8556f4672e2d63e0c6bb249f87
Vyacheslav Sidorov <vvsydc@mail.missouri.edu>
2018-12-26 16:05:03 -0600

    Added a few more testing components and imports for AppRouting and Forms

--- lotus-website/src/app/app.module.ts
+++ lotus-website/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { TestComponent } from './components/test/test.component';
-import { Test2Component } from './test2/test2.component';
+import { Test2Component } from './components/test2/test2.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { Test2Component } from './test2/test2.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
