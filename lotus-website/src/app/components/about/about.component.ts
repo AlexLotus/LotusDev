e7fdca3dad50e12998c56131dce38bf02adf2918
GlacierDev <konak99@yahoo.com>
2018-12-28 00:45:15 -0600

    hey

--- lotus-website/src/app/components/about/about.component.ts
+++ lotus-website/src/app/components/about/about.component.ts
@@ -1,75 +1,13 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  animations: [
-    trigger('openClose', [
-      // ...
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.5,
-        backgroundColor: 'green'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-    ]),
-    trigger('flyInOut', [
-      state('in', style({ transform: 'translateX(0)' })),
-      transition('void => *', [
-        style({ transform: 'translateX(-100%)' }),
-        animate(100)
-      ]),
-      transition('* => void', [
-        animate(100, style({ transform: 'translateX(100%)' }))
-      ])
-    ]),
-    trigger('changeDivSize', [
-      state('initial', style({
-        backgroundColor: 'green',
-        width: '100px',
-        height: '100px'
-      })),
-      state('final', style({
-        backgroundColor: 'red',
-        width: '200px',
-        height: '200px'
-      })),
-      transition('initial=>final', animate('1500ms')),
-      transition('final=>initial', animate('1000ms'))
-    ]),
-  ]
+
 })
 export class AboutComponent implements OnInit {
   isOpen = true;
-
-  toggle() {
-    this.isOpen = !this.isOpen;
-  }
-
-  currentState = 'initial';
-
-  changeState() {
-    this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
-  }
 
   constructor() { }
 
